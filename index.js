2b28afd14561b6a95b79d1dddc97714ee6127525
Nils P. Ellingsen <nils@deviant.no>
2016-06-30 13:48:49 +0200

    fix path for express

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + './'));
 
 var poort = process.env.PORT || 3000;
 app.listen(poort);
