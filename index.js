6078506500cb9838ca31402a922f833dcd2feab9
Nils P. Ellingsen <nils@deviant.no>
2016-06-30 13:46:18 +0200

    use express

---
+++ index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+var poort = process.env.PORT || 3000;
+app.listen(poort);
+
+console.log('Server started on port ' + poort);
